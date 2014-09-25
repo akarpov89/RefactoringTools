69d5f7295405f8a67c9b6aa9a0ad71a7f150f5a5
Andrew Karpov <andrew.karpov@hotmail.com>
2014-09-25 17:22:33 +0400

    started working on chain method calls

--- RefactoringTools/RefactoringTools/ChainMethodCallsRefactoringProvider.cs
+++ RefactoringTools/RefactoringTools/ChainMethodCallsRefactoringProvider.cs
@@ -8,17 +8,30 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.Text;
 using System.Threading;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace RefactoringTools
 {
-    //[ExportCodeRefactoringProvider(RefactoringId, LanguageNames.CSharp)]
+    [ExportCodeRefactoringProvider(RefactoringId, LanguageNames.CSharp)]
     internal class ChainMethodCallsRefactoringProvider : ICodeRefactoringProvider 
     {
         public const string RefactoringId = "ChainMethodsRefactoringProvider";
 
         public async Task<IEnumerable<CodeAction>> GetRefactoringsAsync(Document document, TextSpan span, CancellationToken cancellationToken)
         {
-            return null;            
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+            var node = root.FindNode(span);
+
+            if (!node.IsKind(SyntaxKind.Block))
+                return null;
+
+            var block = (BlockSyntax)node;
+
+            var selectedStatements = block.Statements.Where(s => s.SpanStart >= span.Start && s.SpanStart <= span.End).ToArray();
+
+            return null;
         }
     }
 }
