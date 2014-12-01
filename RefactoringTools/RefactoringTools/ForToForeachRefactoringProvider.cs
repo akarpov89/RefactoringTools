ebb36013a70df9ba5b30dca8cdcee122dd979624
Andrew Karpov <andrew.karpov@hotmail.com>
2014-12-01 13:00:46 +0300

    ForToForeach initial commit

---
+++ RefactoringTools/RefactoringTools/ForToForeachRefactoringProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis.CodeRefactorings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.Text;
+using System.Threading;
+
+namespace RefactoringTools
+{
+    [ExportCodeRefactoringProvider(RefactoringId, LanguageNames.CSharp)]
+    internal class ForToForeachRefactoringProvider : ICodeRefactoringProvider
+    {
+        public const string RefactoringId = nameof(ForToForeachRefactoringProvider);
+
+        public Task<IEnumerable<CodeAction>> GetRefactoringsAsync(Document document, TextSpan span, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
