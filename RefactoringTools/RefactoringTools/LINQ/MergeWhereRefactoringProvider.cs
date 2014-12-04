188c69c0316e0a0e40f258c86dc6e8c7be0fb90d
akarpov89 <andrewcarpov@gmail.com>
2014-12-05 00:11:07 +0300

    Organazing files into folders

---
+++ RefactoringTools/RefactoringTools/LINQ/MergeWhereRefactoringProvider.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Andrew Karpov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringTools
+{
+    class MergeWhereRefactoringProvider
+    {
+    }
+}
